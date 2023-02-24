c38541a695d7259b72fdeea01e2603e36f7da4c7
KyungoeSim <tlaruddhl@gmail.com>
2023-02-24 12:41:28 +0900

    ㅇㅇ

--- GroupTalk/src/jdbc/feedDAO.java
+++ GroupTalk/src/jdbc/feedDAO.java
@@ -16,7 +16,7 @@ import util.ConnectionPool;
 public class feedDAO {
 	
 
-	//회원 가입시 아이디가 이미 존재하는 지 여부 확인
+	
 	public static int insert(String id,String content) throws NamingException, SQLException {
 		
 		Connection conn = null;
@@ -51,6 +51,42 @@ public class feedDAO {
 				
 				conn = ConnectionPool.get();
 				pstmt = conn.prepareStatement(sql);
+				
+				rs = pstmt.executeQuery(); 
+					 
+				JSONArray feeds = new JSONArray();
+				
+				while(rs.next()) {
+					JSONObject obj = new JSONObject();
+					obj.put("no", rs.getString(1));
+					obj.put("id", rs.getString(2));
+					obj.put("content", rs.getString(3));
+					obj.put("ts", rs.getString(4));
+					
+					feeds.add(obj);
+				
+				}return feeds.toJSONString(); //feeds가 JSONArray로 받기 때문에 string로 바꿔주는것
+				
+			}finally {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			}
+		}
+		
+		//내목록 꺼내기
+		public static String getmylist(String id) throws NamingException, SQLException {
+			
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			try {
+				String sql = "SELECT * FROM feed WHERE id=? ORDER BY ts DESC ";
+				
+				conn = ConnectionPool.get();
+				pstmt = conn.prepareStatement(sql);
+					pstmt.setNString(1, id);
 				
 				rs = pstmt.executeQuery(); 
 					 
